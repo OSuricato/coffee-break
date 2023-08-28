e90a97866377ceedf4b1414d6ae06144ee901bb2
Henrique de Mello Franco <henry@Henriques-MacBook-Pro-2.local>
2023-08-28 17:47:35 -0300

    add validations to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :user
+
+  has_many :purchases
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 20 }
+  validates :region, :roast_type, presence: true, length: { minimum: 4 }
+  validates :weight, :price, presence: true
 end
