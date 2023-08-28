a60c921da3713bd83fbaae0ffe6333982e677a87
victor barbosa <barbosavictor30@gmail.com>
2023-08-28 17:34:51 -0300

    added routes for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users do
+    resources :purchases, only: [:index, :create]
+  end
+
+  resources :products do
+    resources :purchases, only: [:index, :create]
+  end
+  root to: "pages#home"
 end
