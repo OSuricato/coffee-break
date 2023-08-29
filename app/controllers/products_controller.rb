070877d979dc6df4c058cc97a26b28a4dca5e891
victor barbosa <barbosavictor30@gmail.com>
2023-08-29 15:27:39 -0300

    added new/create to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,33 @@
 class ProductsController < ApplicationController
+  # index
+  # show
+
+
+  # new
+  def new
+    @product = Product.new
+  end
+
+  # create
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save!
+      redirect_to @product, notice: "Product created succesfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  # update/edit
+  # destroy
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :region, :roast_type, :description, :weight, :price)
+  end
 end
