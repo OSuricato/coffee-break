9ae152fde06e8ebc78e858f1a9c4b81299a6ffa9
victor barbosa <barbosavictor30@gmail.com>
2023-08-29 12:16:19 -0300

    user and product seed complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.destroy_all
+User.destroy_all
+
+puts 'starting seed'
+
+10.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: '123123',
+    first_name: Faker::Name.name,
+    last_name: Faker::Name.last_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    is_company: true,
+    company_name: Faker::Company.name,
+    register_number: Faker::CNPJ.numeric
+  )
+  puts "usuario criado"
+  5.times do
+    product = Product.new(
+      # name
+      name: Faker::Coffee.blend_name,
+      # description
+      description: Faker::Coffee.notes,
+      # region
+      region: Faker::Coffee.origin,
+      weight: [250, 500, 1000].sample,
+      roast_type: ["grain", "coarse", "thin"].sample,
+      price: rand(10..150)
+    )
+    product.user = user
+    product.save!
+  end
+end
+
+40.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: '123123',
+    first_name: Faker::Name.name,
+    last_name: Faker::Name.last_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    is_company: false,
+    company_name: 'client',
+    register_number: Faker::CPF.number
+  )
+  puts "usuario criado"
+end
+
+puts 'seed succesfull'
